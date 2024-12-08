a7a71005e23b9ccbc2eba6114585d7315ef2036e
raymond <rayhon1014@gmail.com>
2024-12-07 23:10:35 -0800

    updated

--- cloudflare-pages/functions/api/post/[[id]].js
+++ cloudflare-pages/functions/api/post/[[id]].js
@@ -1,13 +1,14 @@
-import posts from './data';
+import getPostsData from './data';
 
 export const onRequestGet = async (context) => {
     const id = context.params.id;
     if (!id) {
-        return Response.json({ error: 'No id provided' }, { status: 44 });
+        return Response.json({ error: 'No id provided' }, { status: 400 });
     }
+    const posts = getPostsData();
     const post = posts.find(post => post.id === Number(id));
     if (!post) {
         return Response.json({ error: 'Post not found' }, { status: 404 });
     }
     return Response.json(post);
-}
+};
--- cloudflare-pages/functions/api/posts.js
+++ cloudflare-pages/functions/api/posts.js
@@ -1,5 +1,8 @@
-import posts from './post/data';
+import getPostsData from './post/data';
 
 export const onRequestGet = async () => {
+    console.log('API called: /api/posts at', new Date().toISOString());
+    const posts = getPostsData();
+    console.log('Posts data:', posts);
     return Response.json(posts);
 };
